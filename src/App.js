880088ae11c590b23f42ab85da2f8489b592ff2f
Ryan Jones <RyanMan56@gmail.com>
2019-12-09 18:03:47 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import WSAvcPlayer from 'ws-avc-player';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: null,
+  }
+  wsavc = new WSAvcPlayer({ useWorker: true });
+
+  componentDidMount() {
+    document.getElementById('App').appendChild(this.wsavc.AvcPlayer.canvas);    
+    this.wsavc.connect('ws://192.168.1.156:3000/ws');
+
+    document.addEventListener('visibilitychange', () => {
+      if (document.hidden) {
+        console.log('disconnecting websocket connection');
+        this.wsavc.disconnect();
+      } else {
+        this.wsavc.connect('ws://192.168.1.156:3000/ws');
+        console.log('reconnecting websocket connection');
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App" id="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
