46858f6e60ce9e1180e517913fdaf7fc00ae73a9
Ryan Jones <RyanMan56@gmail.com>
2019-12-14 15:51:51 +0000

    Outstanding changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import WSAvcPlayer from 'ws-avc-player';
+import Peer from 'peerjs';
 import logo from './logo.svg';
 import './App.css';
 
@@ -21,6 +22,22 @@ class App extends Component {
         this.wsavc.connect('ws://192.168.1.156:3000/ws');
         console.log('reconnecting websocket connection');
       }
+    });
+
+    const peer = new Peer();
+    peer.on('open', (id) => {
+      console.log(id);
+    })
+
+    const conn = peer.connect('1');
+    conn.on('open', () => {
+      conn.send('hi');
+    });
+
+    peer.on('connection', (conn) => {
+      conn.on('data', (data) => {
+        console.log(data);
+      })
     })
   }
 
